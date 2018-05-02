401b41908bb6874ea053fa00da4ef073052cded0
jrnold <jeffrey.arnold@gmail.com>
2018-05-01 19:54:28 -0700

    misc

--- bin/weeks-to-days.R
+++ bin/weeks-to-days.R
@@ -2,31 +2,38 @@ library("rmarkdown")
 library("stringr")
 library("tidyverse")
 
-weeks <- dir("content/schedule/", pattern = "week-.*\\.Rmd$",
+weeks <- dir("content/weeks/", pattern = "week-.*\\.Rmd$",
              full.names = TRUE)
-
-path <- weeks[[i]]
-metadata <- yaml_front_matter(path)
-week <- metadata[["week"]]
-for (meeting in metadata[["meetings"]]) {
-  filename <- str_c(meeting[["date"]], "-",
-                    if (meeting[["lab"]]) "lab" else "class",
+for (path in weeks) {
+  metadata <- yaml_front_matter(path)
+  week <- metadata[["week"]]
+  for (meeting in metadata[["meetings"]]) {
+    filename <- str_c(meeting[["date"]], "-",
+                      if (meeting[["lab"]]) "lab" else "class",
+                      ".Rmd")
+    title <- str_c(format(as.Date(meeting[["date"]]), "%b, %d (%a)"), "&mdash;",
+                    if (meeting[["lab"]]) "Lab" else "Class",
                     ".Rmd")
-  meeting[["week"]] <- week
-  if (meeting[["lab"]]) {
-    meeting[["startTime"]] <- str_c(format(meeting[["date"]]),
-                                    "T13:30:00")
-    meeting[["endTime"]] <- str_c(format(meeting[["date"]]),
-                                  "T14:20:00")
-  } else {
-    meeting[["startTime"]] <- str_c(format(meeting[["date"]]),
-                                    "T14:30:00")
-    meeting[["endTime"]] <- str_c(format(meeting[["date"]]),
-                                  "T15:50:00")
+    meeting[["title"]] <- title
+    meeting[["week"]] <- week
+    meeting[["tags"]] <- NULL
+    meeting[["readings"]] <- map(meeting[["before"]], "title")
+    meeting[["before"]] <- NULL
+    meeting[["optional"]] <- map(meeting[["after"]], "title")
+    meeting[["after"]] <- NULL
+    if (meeting[["lab"]]) {
+      meeting[["startTime"]] <- str_c(format(meeting[["date"]]),
+                                      "T13:30:00")
+      meeting[["endTime"]] <- str_c(format(meeting[["date"]]),
+                                    "T14:20:00")
+    } else {
+      meeting[["startTime"]] <- str_c(format(meeting[["date"]]),
+                                      "T14:30:00")
+      meeting[["endTime"]] <- str_c(format(meeting[["date"]]),
+                                    "T15:50:00")
+    }
+    meeting[["date"]] <- meeting[["startTime"]]
+    filestr <- str_c("---\n", yaml::as.yaml(meeting), "\n---\n")
+    cat(filestr, file = file.path("content", "schedule", filename))
   }
-  filestr <- str_c("---\n", yaml::as.yaml(meeting), "\n---\n")
-  cat(filestr, file = file.path("content", "schedule2", filename))
 }
-
-
-
